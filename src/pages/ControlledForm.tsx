304b3c882ce46b3bbe7f07894263778451389981
Tiago Santos <tfsantosbr@gmail.com>
2022-09-10 23:23:52 -0300

    add exemplos de controlled e uncontrolled form

--- src/pages/ControlledForm.tsx
+++ src/pages/ControlledForm.tsx
@@ -1,7 +1,29 @@
+import { useState } from 'react';
+
 export function ControlledForm() {
+  const [name, setName] = useState('');
+
   return (
     <>
       <h1>Controlled Form</h1>
+
+      <form action=''>
+        
+        <label htmlFor='name'>Name: </label>
+        <input
+          id='name'
+          type='text'
+          onChange={(e) => setName(e.target.value)}
+          value={name}
+        />
+
+        <br />
+
+        <button type='submit' disabled={!name}>
+          Enviar
+        </button>
+
+      </form>
     </>
   );
 }
