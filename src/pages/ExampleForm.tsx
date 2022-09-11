28ea346cf11e8d5e2038ef7301b5ab7664598951
Tiago Santos <tfsantosbr@gmail.com>
2022-09-11 00:24:55 -0300

    add validações

--- src/pages/ExampleForm.tsx
+++ src/pages/ExampleForm.tsx
@@ -1,12 +1,26 @@
 import { useState } from 'react';
 import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import * as zod from 'zod';
+
+const newDefaultFormValidationSchema = zod.object({
+  name: zod.string().min(1, 'Name is required my friend!!!'),
+  email: zod.string().min(1),
+  password: zod.string().min(1),
+  age: zod.number().min(1).max(100),
+  birthdate: zod.date(),
+});
 
 export function ExampleForm() {
-  const { register, handleSubmit, watch } = useForm();
+  const { register, handleSubmit, watch, formState } = useForm({
+    resolver: zodResolver(newDefaultFormValidationSchema),
+  });
 
   function handleFormSubmit(data: any) {
     console.log(data);
   }
+
+  console.log(formState.errors);
 
   const name = watch('name');
   const isSubmitButtonDisabled = !name;
@@ -51,7 +65,9 @@ export function ExampleForm() {
 
         <br />
 
-        <button type='submit' disabled={isSubmitButtonDisabled}>Enviar</button>
+        <button type='submit' disabled={isSubmitButtonDisabled}>
+          Enviar
+        </button>
       </form>
     </>
   );
